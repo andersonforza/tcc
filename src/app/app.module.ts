ffbefc52f7fdaf56689fedfdc3cd2f57e06ac7ba
Anderson Forza <andersonforza@gmail.com>
2021-06-05 21:55:29 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { WebcamModule } from 'ngx-webcam';
 
 import { AppComponent } from './app.component';
+import { CameraComponent } from './views/camera/camera.component';
+import { ResultComponent } from './views/result/result.component';
+import { UploadComponent } from './views/upload/upload.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    CameraComponent,
+    ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
